d74d0868d92dab5c0810fd1ef54fe078c0f8dcbc
creaexo <creaexo@gmail.com>
2025-08-02 03:04:25 +0500

    refactor: Settings class inherits by pydantic BaseSettings class.

--- config.py
+++ config.py
@@ -1,34 +1,37 @@
 from typing import Literal
-from pydantic import BaseModel, Field
 
-class Settings(BaseModel):
+from pydantic import Field
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
     # Server
-    host: str = Field("0.0.0.0", description="API host")
-    port: int = Field(8000, description="API port")
+    host: str = Field(default="0.0.0.0", description="API host")
+    port: int = Field(default=8000, description="API port")
 
     # Database
     db_backend: Literal["postgresql", "sqlcipher"] = Field(
-        "postgresql", description="Choose 'postgresql' or 'sqlcipher'"
+        default="postgresql", description="Choose 'postgresql' or 'sqlcipher'"
     )
     postgres_dsn: str | None= Field(
-        "postgresql://user:password@localhost:5432/passwords_db",
+        default="postgresql://user:password@localhost:5432/passwords_db",
         description="PostgreSQL DSN"
     )
     sqlcipher_file: str | None= Field(
-        "db/sqlcipher.sqlite",
+        default="db/sqlcipher.sqlite",
         description="Path to SQLCipher-encrypted SQLite file"
     )
 
     # Crypto
-    kdf_salt_size: int = Field(16, description="Salt size for KDF in bytes")
-    kdf_iterations: int = Field(100_000, description="PBKDF2 iterations")
+    kdf_salt_size: int = Field(default=16, description="Salt size for KDF in bytes")
+    kdf_iterations: int = Field(default=100_000, description="PBKDF2 iterations")
     encryption_algorithm: Literal["AESGCM"] = Field(
-        "AESGCM", description="Symmetric encryption algorithm"
+        default="AESGCM", description="Symmetric encryption algorithm"
     )
 
     # Auth
-    secret_key: str = Field(..., description="Secret key for JWT tokens")
-    token_expire_minutes: int = Field(60 * 24, description="JWT expiration time")
+    secret_key: str = Field(default=..., description="Secret key for JWT tokens")
+    token_expire_minutes: int = Field(default=60 * 24, description="JWT expiration time")
 
     class Config:
         env_file = ".env"
